ed136e3f1c8d31dd9b083ab5e614dbb3342796fb
wxb <13651269612@163.com>
2018-01-26 14:00:43 +0800

    add custum application

---
+++ app/src/main/java/com/wxb/app/App.java
@@ -0,0 +1,15 @@
+package com.wxb.app;
+
+import android.app.Application;
+
+/**
+ * Created by wuxiaobo on 2018/1/26.
+ * application
+ */
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
