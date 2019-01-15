84c2ccb998573bb1c27d047c86a2dc09fd93ef05
wuxiaobo <wuxiaobo@xinzhili.cn>
2019-01-15 14:58:33 +0800

    add mvp-model 测试

--- app/src/main/java/com/wxb/mvp/base/BaseSrcModel.java
+++ app/src/main/java/com/wxb/mvp/base/BaseSrcModel.java
@@ -2,10 +2,13 @@ package com.wxb.mvp.base;
 
 import com.wxb.BuildConfig;
 
+/**
+ * mvp-model基类，添加方法 判断 用ali还是ibm接口
+ */
 public class BaseSrcModel implements BaseContract.BaseModel {
 
     @Override
     public boolean isAli() {
-        return BuildConfig.IS_ALI;
+        return BuildConfig.IS_ALI;//build.gradle中配置
     }
 }
