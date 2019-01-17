48634c4e72e85ed4e2637e2d5fec7cb805973cee
wuxiaobo <wuxiaobo@xinzhili.cn>
2019-01-17 18:21:48 +0800

    update

--- app/src/main/java/com/wxb/mvp/ui/activity/SplashActivity.java
+++ app/src/main/java/com/wxb/mvp/ui/activity/SplashActivity.java
@@ -1,12 +1,8 @@
 package com.wxb.mvp.ui.activity;
 
-import android.os.Handler;
 import android.view.KeyEvent;
 
-import com.wxb.MainActivity;
-import com.wxb.R;
 import com.wxb.app.utils.Dlog;
-import com.wxb.app.utils.ToastUtil;
 import com.wxb.mvp.ui.activity.login.LoginActivity;
 import com.wxb.mvp.ui.base.BaseActivity;
 import com.wxb.mvp.view.StatusBarUtil;
@@ -17,13 +13,9 @@ import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
-import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class SplashActivity extends BaseActivity {
@@ -59,19 +51,23 @@ public class SplashActivity extends BaseActivity {
 //        countdownTest();
         final int count = 3;
         //可以延时执行任务，可以做倒计时
-        Disposable disposable = Flowable.intervalRange(0, count, 0, 1,TimeUnit.SECONDS)
-                .map(aLong -> {return count - aLong;})
+        Disposable disposable = Flowable.intervalRange(0, count, 0, 1, TimeUnit.SECONDS)
+                .map(aLong -> {
+                    return count - aLong;
+                })
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnNext(aLong -> {Dlog.e(">>>>>>>>>> countdown is " + (aLong));})
+                .doOnNext(aLong -> {
+                    Dlog.e(">>>>>>>>>> countdown is " + (aLong));
+                })
                 .doOnComplete(this::jump2login)
                 .subscribe();
 
         mCompositeDisposable.add(disposable);
     }
 
-    private void countdownTest(){
+    private void countdownTest() {
         //interval 是会无限发送事件的。第一个参数开始的延时事件，二个参数为时间间隔，三个时间间隔单元（单位）
-        Observable.interval(0,1, TimeUnit.SECONDS)
+        Observable.interval(0, 1, TimeUnit.SECONDS)
                 //因为interval是无限发送的，所以使用take操作符，只会发送10个数据
                 .take(5)
                 //因为interval发送是从0开始的，而我们要使用的是10 - value 所以用map做一下转换
